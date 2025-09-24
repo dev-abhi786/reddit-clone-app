b2e306027f1de8121b77d5e9dfd916bb8df61dc4
dev-aomine <155187697+dev-aomine@users.noreply.github.com>
2025-09-24 15:56:18 +0530

    bottom tabs setup and home screen created with static data

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { FlatList, Text, View } from "react-native";
+import PostListItem from "../components/PostListItem";
+import posts from "../../../assets/data/posts.json";
+
+export default function HomeScreen() {
+  return (
+    <FlatList
+      data={posts}
+      renderItem={({ item }) => <PostListItem post={item} />}
+    />
+  );
+}
